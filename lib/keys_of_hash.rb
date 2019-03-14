e7fd4e00b5807e569e5939327ca58ec2e0c68dd4
Ferris Boran <ferris.boran@gmail.com>
2019-03-14 18:26:02 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,11 @@ class Hash
   def keys_of(*arguments)
     arr = []
     arguments.each do |att|
-      arr.push(att)
+      this.each do |key, value|
+        if att == value
+          arr.push(key)
+        end
+      end
     end
     return arr
   end
